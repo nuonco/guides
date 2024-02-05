c0b94af8f99756db2a0a6bd54a2f970887f51eb5
Jordan Acosta <jordanjacosta@gmail.com>
2024-02-05 14:03:56 -0800

    feat(deploy-helm-to-aws-eks): add components

---
+++ aws-eks-tutorial/components/docker-image/discover.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/nuonco/guides/deploy-to-aws-ecs/components/docker-image/internal/introspection"
+)
+
+type discoverEndpoint struct {
+	Path        string `json:"path"`
+	Description string `json:"description"`
+}
+
+type discoverResponse struct {
+	Description string             `json:"description"`
+	Endpoints   []discoverEndpoint `json:"endpoints"`
+}
+
+func discoverHandler(ctx *gin.Context) {
+	resp := &discoverResponse{
+		Description: "This api exposes introspection details of a Nuon app running in a customer's cloud account.",
+		Endpoints: []discoverEndpoint{
+			{
+				Description: introspection.EnvDescription,
+				Path:        "/introspect/env",
+			},
+			{
+				Description: introspection.TerraformDescription,
+				Path:        "/introspect/terraform",
+			},
+			{
+				Description: introspection.SecretsDescription,
+				Path:        "/introspect/secrets",
+			},
+			{
+				Description: introspection.DefaultsDescription,
+				Path:        "/introspect/defaults",
+			},
+			{
+				Description: introspection.SandboxDescription,
+				Path:        "/introspect/sandbox",
+			},
+			{
+				Description: introspection.NuonDescription,
+				Path:        "/introspect/nuon",
+			},
+			{
+				Description: introspection.DockerBuildDescription,
+				Path:        "/introspect/docker-build",
+			},
+			{
+				Description: introspection.ExternalImageDescription,
+				Path:        "/introspect/external-image",
+			},
+			{
+				Description: "/livez check",
+				Path:        "/livez",
+			},
+			{
+				Description: "/readyz check",
+				Path:        "/readyz",
+			},
+		},
+	}
+
+	ctx.JSON(http.StatusOK, resp)
+}
---
+++ aws-eks-tutorial/components/helm-chart/templates/api_certificate.tpl
@@ -0,0 +1,15 @@
+---
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: {{ include "common.fullname" . }}-public
+  namespace: {{ .Release.Namespace }}
+  labels:
+    {{- include "common.apiLabels" . | nindent 4 }}
+spec:
+  secretName: api-ingress-public-tls
+  dnsNames:
+    - {{ .Values.api.ingresses.public_domain }}
+  issuerRef:
+    name: public-issuer
+    kind: ClusterIssuer
