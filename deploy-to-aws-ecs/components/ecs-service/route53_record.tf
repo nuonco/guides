7fe9a4732d21e6d33df4ec460fb5b1f7dc1bf829
Jordan Acosta <jordanjacosta@gmail.com>
2024-02-07 07:53:17 -0800

    fix(deploy-to-aws-ecs): forgot CNAME record

---
+++ deploy-to-aws-ecs/components/ecs-service/route53_record.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "cname" {
+  zone_id = module.ingress.zone_id
+  name    = var.domain_name
+  type    = "CNAME"
+  ttl     = 300
+  records = [module.ingress.dns_name]
+}
