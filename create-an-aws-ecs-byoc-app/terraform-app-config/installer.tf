bdf84e0881a757142512af762019d61925a20728
Jordan Acosta <jordanjacosta@gmail.com>
2024-02-29 11:42:59 -0800

    feat(aws-ecs): add config file example

--- create-an-aws-ecs-byoc-app/terraform-app-config/installer.tf
+++ create-an-aws-ecs-byoc-app/terraform-app-config/installer.tf
@@ -2,7 +2,7 @@ resource "nuon_app_installer" "my_ecs_app" {
   app_id            = nuon_app.my_ecs_app.id
   name              = "My ECS App"
   description       = "A demo app that runs on ECS."
-  slug              = nuon_app.my_ecs_app.name
+  slug              = "my-ecs-app"
   documentation_url = "https://docs.nuon.co/"
   community_url     = "https://join.slack.com/t/nuoncommunity/shared_invite/zt-1q323vw9z-C8ztRP~HfWjZx6AXi50VRA"
   logo_url          = "https://assets-global.website-files.com/62a2c1332b518a9eedc6de2f/651df2030c43865b9b16046b_Group%2048.png"
