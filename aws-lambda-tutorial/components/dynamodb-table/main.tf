af0de7e8ff2efa1bce78af5223b82f3deacafb52
Jordan Acosta <jordanjacosta@gmail.com>
2024-08-06 13:14:01 -0700

    fix(aws-lambda-tutorial): pin modules versions

--- aws-lambda-tutorial/components/dynamodb-table/main.tf
+++ aws-lambda-tutorial/components/dynamodb-table/main.tf
@@ -1,5 +1,6 @@
 module "dynamodb_table" {
-  source = "terraform-aws-modules/dynamodb-table/aws"
+  source  = "terraform-aws-modules/dynamodb-table/aws"
+  version = "~> 4.0.0"
 
   name     = var.name
   hash_key = var.hash_key
